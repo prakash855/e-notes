cb6b29045f4c4fbad61e8f99ef70865e064eb62b
prakash855 <sshivamprakashh301@gmail.com>
2024-01-12 20:22:54 +0530

    feat: create route created

--- server/controllers/notes.js
+++ server/controllers/notes.js
@@ -2,18 +2,20 @@ import { Note } from "../models/notes.js";
 
 export const getNotes = async (req, res) => {
   try {
-  } catch (error) {
     const Notes = await Note.find();
     res.status(200).json(Notes);
+  } catch (error) {
     res.status(404).json({ message: error.message });
   }
 };
 
 export const createNotes = async (req, res) => {
+  const note = req.body;
+  const newNote = new Note(note);
   try {
+    await newNote.save();
+    res.status(201).json(newNote);
   } catch (error) {
-    const Notes = await Note.find();
-    res.status(200).json(Notes);
-    res.status(404).json({ message: error.message });
+    res.status(409).json({ message: error.message });
   }
 };

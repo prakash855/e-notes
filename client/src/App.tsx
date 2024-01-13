07fb032ee54fd0cb1081405e7ade014d2479e425
prakash855 <sshivamprakashh301@gmail.com>
2024-01-13 22:56:44 +0530

    refactor: typescript prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,15 @@
-import { useSelector } from 'react-redux';
-import './App.css'
+import { useSelector } from "react-redux";
+import "./App.css";
 
 function App() {
-  const state = useSelector((state => state))
-  console.log(state)
+  const state = useSelector((state) => state);
+  console.log(state);
 
   return (
     <>
-      <h1 className="text-3xl text-center font-bold">
-        Hello world!
-      </h1>
+      <h1 className="text-3xl text-center font-bold">Hello world!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

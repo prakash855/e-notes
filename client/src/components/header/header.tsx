dea4e5ef99c7551ca37cab99d52b88723220e916
prakash855 <sshivamprakashh301@gmail.com>
2024-01-14 21:59:47 +0530

    feat: create form

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -11,12 +11,27 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Textarea,
 } from "@chakra-ui/react";
 
 import CreateAndUpdateModal from "../modal/create-and-update-modal";
 
+const initialState = {
+  title: "",
+  content: "",
+  backgroundColor: "",
+  isArchived: false,
+};
+
 const Header: FC = () => {
+  const [notesState, setNotesState] = useState(initialState);
   const [submitted, setSubmitted] = useState<boolean>(false);
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    console.log(notesState);
+    setNotesState(initialState);
+  };
 
   return (
     <div className="flex justify-end m-5">
@@ -27,22 +42,34 @@ const Header: FC = () => {
       >
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Create your account</ModalHeader>
+          <ModalHeader>Create your note</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
             <FormControl>
-              <FormLabel>First name</FormLabel>
-              <Input placeholder="First name" />
+              <FormLabel>Title</FormLabel>
+              <Input
+                placeholder="Add Title"
+                value={notesState.title}
+                onChange={(e) =>
+                  setNotesState({ ...notesState, title: e.target.value })
+                }
+              />
             </FormControl>
 
             <FormControl mt={4}>
-              <FormLabel>Last name</FormLabel>
-              <Input placeholder="Last name" />
+              <FormLabel>Description</FormLabel>
+              <Textarea
+                value={notesState.content}
+                onChange={(e) =>
+                  setNotesState({ ...notesState, content: e.target.value })
+                }
+                placeholder="Add Description"
+              />
             </FormControl>
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="blue" mr={3}>
+            <Button onClick={handleSubmit} colorScheme="blue" mr={3}>
               Save
             </Button>
             <Button onClick={() => setSubmitted(false)}>Cancel</Button>

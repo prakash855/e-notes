8308ecc3465be82368834b062867f8df28375bf7
prakash855 <sshivamprakashh301@gmail.com>
2024-01-16 13:33:52 +0530

    feat: archive page

--- client/src/sections/body.tsx
+++ client/src/sections/body.tsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import NoteCard from "../components/note-card/note-card";
 import { fetchNotes, Note } from "../slices/services";
 import { AppDispatch, RootState } from "../store";
 
 export const Body = () => {
+  const { pathname } = useLocation();
   const dispatch = useDispatch<AppDispatch>();
   const { notes } = useSelector((state: RootState) => state);
 
@@ -15,7 +17,9 @@ export const Body = () => {
   return (
     <div className="flex gap-5 flex-wrap my-4">
       {notes?.notes
-        ?.filter((note: Note) => !note.isArchived)
+        ?.filter((note: Note) =>
+          pathname === `/` ? !note.isArchived : note.isArchived
+        )
         ?.map((note: Note) => (
           <NoteCard key={note._id} {...note} />
         ))}

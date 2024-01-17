37884e0d0f59cdef374a4f2e037b40348396bd90
prakash855 <sshivamprakashh301@gmail.com>
2024-01-17 18:40:26 +0530

    feat: cors error fixed

--- server/index.js
+++ server/index.js
@@ -11,7 +11,13 @@ dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "React app URL",
+    methods: "GET, POST, PUT, DELETE",
+    allowedHeaders: "Content-Type",
+  })
+);
 
 app.use("/notes", notesRoutes);
 

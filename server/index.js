0ec2878223a691302973cf3a4596ab593aee9414
prakash855 <sshivamprakashh301@gmail.com>
2024-01-17 18:47:35 +0530

    fix: back to basics

--- server/index.js
+++ server/index.js
@@ -12,12 +12,6 @@ dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "React app URL");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
 
 app.use("/notes", notesRoutes);
 

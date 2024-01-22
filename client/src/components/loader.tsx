ca6f2e70a978c9ff8ad3a1460a9343ed8b27a95a
prakash855 <sshivamprakashh301@gmail.com>
2024-01-22 18:05:17 +0530

    refactor

--- client/src/components/loader.tsx
+++ client/src/components/loader.tsx
@@ -1,11 +1,9 @@
 import { Spinner } from "@chakra-ui/react";
 
-const Loader = () => {
-  return (
-    <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <Spinner />
-    </div>
-  );
-};
+const Loader = () => (
+  <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+    <Spinner />
+  </div>
+);
 
 export default Loader;
--- client/src/components/sidebar-link/sidebar-link.tsx
+++ client/src/components/sidebar-link/sidebar-link.tsx
@@ -1,17 +1,15 @@
 import { SidebarConfigItem } from "../../sections/sidebar";
 
-const SidebarLink = ({ icon: Icon, label }: SidebarConfigItem) => {
-  return (
-    <div
-      className="flex rounded py-3 font-semibold cursor-pointer hover:bg-slate-300 items-center"
-      style={{ borderRadius: "0 25px 25px 0" }}
-    >
-      <div className="mx-5">
-        <Icon />
-      </div>
-      <div>{label}</div>
+const SidebarLink = ({ icon: Icon, label }: SidebarConfigItem) => (
+  <div
+    className="flex rounded py-3 font-semibold cursor-pointer hover:bg-slate-300 items-center"
+    style={{ borderRadius: "0 25px 25px 0" }}
+  >
+    <div className="mx-5">
+      <Icon />
     </div>
-  );
-};
+    <div>{label}</div>
+  </div>
+);
 
 export default SidebarLink;

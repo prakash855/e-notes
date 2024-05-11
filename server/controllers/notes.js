7209653e98203e48f9d3a119b987444bbf64cd56
prakash855 <sshivamprakashh301@gmail.com>
2024-05-11 08:10:25 +0530

    fix: res passed inside utility

--- server/controllers/notes.js
+++ server/controllers/notes.js
@@ -11,7 +11,7 @@ export const getNotes = async (_, res) => {
 };
 
 export const getNotesById = async ({ params: { id } }, res) => {
-  unknowNoteHandler(id);
+  unknowNoteHandler(id, res);
   try {
     const note = await Note.findById(id);
 
@@ -36,7 +36,7 @@ export const createNotes = async ({ body }, res) => {
 };
 
 export const deleteNotes = async ({ params: { id } }, res) => {
-  unknowNoteHandler(id);
+  unknowNoteHandler(id, res);
   await Note.findByIdAndDelete(id);
   res.json({ message: `Notes deleted successfully!` });
 };
@@ -44,7 +44,7 @@ export const deleteNotes = async ({ params: { id } }, res) => {
 export const updateNotes = async ({ params }, res) => {
   const { id: _id } = params;
   const notes = req.body;
-  unknowNoteHandler(_id);
+  unknowNoteHandler(_id, res);
   const updatedNotes = await Note.findByIdAndUpdate(
     _id,
     { ...notes, _id },
@@ -55,7 +55,7 @@ export const updateNotes = async ({ params }, res) => {
 
 export const archiveNotes = async ({ params }, res) => {
   const { id: _id } = params;
-  unknowNoteHandler(_id);
+  unknowNoteHandler(_id, res);
   try {
     const note = await Note.findById(_id);
     if (!note) {
@@ -76,7 +76,7 @@ export const archiveNotes = async ({ params }, res) => {
 };
 
 export const pinNotes = async ({ params: { id } }, res) => {
-  unknowNoteHandler(id);
+  unknowNoteHandler(id, res);
   try {
     const note = await Note.findById(id);
     if (!note) return res.status(404).send(`No note with this ID`);
--- server/utils.js
+++ server/utils.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export const unknowNoteHandler = (id) => {
+export const unknowNoteHandler = (id, res) => {
   if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).send(`No note with this ID`);
 };
